d0bf3ae3f651bec949e54c91008e4ee4dd969f27
Crashim03 <alexteloduarte@gmail.com>
2022-03-16 19:40:43 +0000

    Newspaper logic

--- Assets/Scripts/Newspaper/News.cs
+++ Assets/Scripts/Newspaper/News.cs
@@ -7,9 +7,9 @@ using TMPro;
 [CreateAssetMenu(fileName = "News", menuName = "ScriptableObjects/News", order = 2)]
 public class News : ScriptableObject
 {
-    [SerializeField] private Image _image;
+    [SerializeField] private Sprite _sprite;
     [SerializeField] private string _text;
 
-    public Image image{get => _image; set => _image = value; }
+    public Sprite sprite{get => _sprite; set => _sprite = value; }
     public string text{get => _text; set => _text = value;}
 }
