1d184112534da20cca3f7b2fbef14b6228bb0d85
HerouFenix <diogo0408@gmail.com>
2022-03-20 19:33:49 +0000

    tutorial

--- Assets/Scripts/TutorialController.cs
+++ Assets/Scripts/TutorialController.cs
@@ -1,21 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TutorialController : MonoBehaviour
 {
     public List<Sprite> tutorialScreens = new List<Sprite>();
-    public SpriteRenderer renderer;
+    public Image imageRenderer;
+    public GameObject menuScreen;
+    public GameObject tutorialScreen;
     private int currentIndex = 0;
+
     private void OnEnable()
     {
         currentIndex = 0;
-        renderer.sprite = tutorialScreens[currentIndex];
+        imageRenderer.sprite = tutorialScreens[currentIndex];
     }
 
     public void GoToNext()
     {
         currentIndex++;
-        renderer.sprite = tutorialScreens[currentIndex];
+        if(currentIndex >= tutorialScreens.Count)
+        {
+            menuScreen.SetActive(true);
+            tutorialScreen.SetActive(false);
+        }
+        imageRenderer.sprite = tutorialScreens[currentIndex];
     }
 }
